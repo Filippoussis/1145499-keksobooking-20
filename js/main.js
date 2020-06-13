6e514f1f14d9ccb6a46102e668ed4b5ddeb28a0e
Filippoussis <filippoussis@iMac-AndrosPhilippos.local>
2020-06-13 11:37:44 +0500

    создал функцию рендера фотографий жилья

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@ var generateDataAd = function () {
       checkout: getRandomElementFromArray(CHECKIN_OUTS),
       features: getRandomArray(FEATURES),
       description: getRandomElementFromArray(DESCRIPTIONS),
-      photos: getRandomElementFromArray(PHOTOS),
+      photos: getRandomArray(PHOTOS),
     },
     location: {
       x: positionX,
@@ -201,7 +201,27 @@ var mapPins = document.querySelector('.map__pins');
 mapPins.append(pinFragment);
 
 var cardTemplate = document.querySelector('#card').content;
+var photoTemplate = cardTemplate.querySelector('.popup__photo');
 
+/**
+ * отрисовка фотографий объявления
+ * @param {object} ad - объект карточки объявления
+ * @param {number} i - индекс элемента в массиве
+ * @return {HTMLElement} возвращает кастомизированный HTMLElement на основе склонированного шаблона
+ */
+var renderPhotos = function (ad, i) {
+  var photo = photoTemplate.cloneNode(true);
+
+  photo.src = ad.offer.photos[i];
+
+  return photo;
+};
+
+/**
+ * отрисовка карточки объявления
+ * @param {object} ad - объект карточки объявления
+ * @return {HTMLElement} возвращает кастомизированный HTMLElement на основе склонированного шаблона
+ */
 var renderCard = function (ad) {
   var card = cardTemplate.cloneNode(true);
   card.querySelector('.popup__title').textContent = ad.offer.title;
@@ -210,6 +230,13 @@ var renderCard = function (ad) {
   card.querySelector('.popup__type').textContent = TYPES[ad.offer.type];
   card.querySelector('.popup__text--capacity').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
   card.querySelector('.popup__text--time').textContent = 'Заезд после ' + ad.offer.checkin + ',' + ' выезд до ' + ad.offer.checkout;
+  card.querySelector('.popup__description').textContent = ad.offer.description;
+  card.querySelector('.popup__avatar').src = ad.author.avatar;
+
+  if (!ad.offer.features.length) {
+    card.querySelector('.popup__features').remove();
+  }
+
   card.querySelectorAll('.popup__feature').forEach(function (featureElement) {
     if (!featureElement.className.split('--').find(function (item) {
       return ad.offer.features.includes(item);
@@ -217,9 +244,16 @@ var renderCard = function (ad) {
       featureElement.remove();
     }
   });
-  card.querySelector('.popup__description').textContent = ad.offer.description;
-  // card.querySelector('.popup__photos').append(getCardPhotos(ad));
-  card.querySelector('.popup__avatar').src = ad.author.avatar;
+
+  for (var i = 0; i < ad.offer.photos.length; i++) {
+    card.querySelector('.popup__photos').append(renderPhotos(ad, i));
+  }
+
+  if (!ad.offer.photos.length) {
+    card.querySelector('.popup__photos').remove();
+  } else {
+    card.querySelector('.popup__photo').remove();
+  }
 
   return card;
 };
