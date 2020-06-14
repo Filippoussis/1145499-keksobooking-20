2d9be61c2be540fb1e6805d5939ccd8fcbad93e4
Filippoussis <filippoussis@iMac-AndrosPhilippos.local>
2020-06-14 19:01:58 +0500

    Валидация селектов Количество комнат - Количество гостей

--- js/main.js
+++ js/main.js
@@ -308,3 +308,36 @@ var activateMode = function () {
 
 mainPin.addEventListener('mousedown', onPressMainPin);
 mainPin.addEventListener('keydown', onPressMainPin);
+
+// Валидация
+
+var numberRooms = adForm.querySelector('#room_number');
+var capacity = adForm.querySelector('#capacity');
+
+numberRooms.addEventListener('change', function () {
+  capacity.setCustomValidity('');
+  if (Number(numberRooms.value) < Number(capacity.value)) {
+    numberRooms.setCustomValidity('Количество комнат должно быть больше или равно количеству гостей');
+  } else if (Number(numberRooms.value) === 100 && Number(capacity.value) !== 0) {
+    numberRooms.setCustomValidity('100 комнат не предназначены для гостей');
+  } else if (Number(numberRooms.value) !== 100 && Number(capacity.value) === 0) {
+    numberRooms.setCustomValidity('Комнаты предназначены только для гостей');
+  } else {
+    numberRooms.setCustomValidity('');
+  }
+  numberRooms.reportValidity();
+});
+
+capacity.addEventListener('change', function () {
+  numberRooms.setCustomValidity('');
+  if (Number(capacity.value) > Number(numberRooms.value)) {
+    capacity.setCustomValidity('Количество гостей должно быть меньше или равно количеству комнат');
+  } else if (Number(capacity.value) === 0 && Number(numberRooms.value) !== 100) {
+    capacity.setCustomValidity('Не для гостей предназначены 100 комнат');
+  } else if (Number(capacity.value) !== 0 && Number(numberRooms.value) === 100) {
+    capacity.setCustomValidity('Гостям не предназначены 100 комнат');
+  } else {
+    capacity.setCustomValidity('');
+  }
+  capacity.reportValidity();
+});
