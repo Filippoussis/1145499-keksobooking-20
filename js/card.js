8b6db10d22721da3f6a3b6ae301ede85ae14e699
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-07-11 09:32:50 +0500

    naming in card.js

--- js/card.js
+++ js/card.js
@@ -5,7 +5,7 @@
   var CARD_TEMPLATE = document.querySelector('#card').content;
   var PHOTO_TEMPLATE = CARD_TEMPLATE.querySelector('.popup__photo');
 
-  var TYPE_HOUSE = {
+  var typeHouse = {
     palace: 'Дворец',
     flat: 'Квартира',
     house: 'Дом',
@@ -34,7 +34,7 @@
     card.querySelector('.popup__title').textContent = ad.offer.title;
     card.querySelector('.popup__text--address').textContent = ad.offer.address;
     card.querySelector('.popup__text--price').textContent = ad.offer.price + '\u20BD/ночь';
-    card.querySelector('.popup__type').textContent = TYPE_HOUSE[ad.offer.type];
+    card.querySelector('.popup__type').textContent = typeHouse[ad.offer.type];
     card.querySelector('.popup__text--capacity').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
     card.querySelector('.popup__text--time').textContent = 'Заезд после ' + ad.offer.checkin + ',' + ' выезд до ' + ad.offer.checkout;
     card.querySelector('.popup__description').textContent = ad.offer.description;
